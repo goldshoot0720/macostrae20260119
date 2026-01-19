1674d2516473d2ae66e39969e1747cdadb63770b
goldshoot0720 <goldshoot0720@gmail.com>
2026-01-19 22:55:41 +0800

    Initial Commit

---
+++ macostrae20260119/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macostrae20260119
+//
+//  Created by 鋒兄 on 2026/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macostrae20260119/macostrae20260119App.swift
@@ -0,0 +1,17 @@
+//
+//  macostrae20260119App.swift
+//  macostrae20260119
+//
+//  Created by 鋒兄 on 2026/1/19.
+//
+
+import SwiftUI
+
+@main
+struct macostrae20260119App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
